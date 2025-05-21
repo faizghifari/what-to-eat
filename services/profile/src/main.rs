efa3c640259f0c9311d7e0b47dab6ed2d8877817
Mika Davies <mika.davies.nt@gmail.com>
2025-05-21 16:15:41 +0900

    Added preliminary request handling to Profile service
    Implemented request detection and basic handling guide for the profile service.
    
    TODO: Revise request message to include origin. Route requests reliably.

--- services/profile/src/main.rs
+++ services/profile/src/main.rs
@@ -1,9 +1,10 @@
 #![allow(dead_code)]
+#![feature(ascii_char)]
 
 mod interfaces;
 
 use supabase_rs::{SupabaseClient, errors::ErrorTypes as SupabaseError};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 
 enum LocalPorts {
     RecipeRecommend = 5001,
@@ -12,6 +13,7 @@ enum LocalPorts {
     Profile = 5004,
 }
 
+#[derive(PartialEq, Eq)]
 enum RequestCodes {
     GetProfile = 100,
     EditProfile = 200,
@@ -96,26 +98,106 @@ async fn manage_listener(listener: &TcpListener) {
 }
 
 async fn process_requests(listener: &TcpListener) -> std::io::Result<()> {
-    use std::net::{IpAddr, Ipv6Addr};
+    use std::net::SocketAddr;
     log::debug!("[PROFILE] Listening for incoming requests...");
     loop {
         // Accept new incoming connection
-        let (_stream, origin_address) = listener.accept().await?;
-        if origin_address.ip() == IpAddr::V6(Ipv6Addr::LOCALHOST) {
-            // Match call from other services
-            let port: u16 = origin_address.port();
-            if port == LocalPorts::RecipeRecommend as u16 {
-                todo!()
-            } else if port == LocalPorts::MenuRecommend as u16 {
-                todo!()
-            } else if port == LocalPorts::EatTogether as u16 {
-                todo!()
-            } else {
-                log::warn!("[PROFILE] Request received from unexpected local port: {port}");
+        let (stream, origin_address): (TcpStream, SocketAddr) = listener.accept().await?;
+
+        // Read stream
+        let mut message: String = String::new();
+
+        loop {
+            // Wait for socket to be readable
+            stream.readable().await?;
+
+            // Try to read the data, could still fail if readable false positive
+            let mut buf: [u8; 64] = [0; 64];
+            match stream.try_read(&mut buf) {
+                Ok(0) => {
+                    log::debug!("[PROFILE] Finished reading stream!");
+                    if let Some(ascii_slice) = buf.as_ascii() {
+                        message.push_str(ascii_slice.as_str());
+                    }
+                    break;
+                }
+                Ok(n) => {
+                    log::debug!("[PROFILE] Read {n} bytes from incoming stream");
+                    if let Some(ascii_slice) = buf.as_ascii() {
+                        message.push_str(ascii_slice.as_str());
+                    }
+                }
+                Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(e) => {
+                    return Err(e);
+                }
+            }
+        }
+
+        // Parse message
+        if message.is_empty() {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "[PROFILE] Incoming stream empty!",
+            ));
+        }
+        let mut split_message = message.split_terminator("#id#");
+        let request_code: isize = split_message
+            .next()
+            .unwrap_or("0")
+            .parse::<isize>()
+            .unwrap_or(-1);
+
+        let user_id: u32 = split_message
+            .next()
+            .unwrap_or("0")
+            .parse::<u32>()
+            .unwrap_or(0);
+
+        if origin_address.ip() == listener.local_addr()?.ip() {
+            // Match request call from other services
+            match request_code {
+                request if request == RequestCodes::GetProfile as isize => {
+                    log::debug!("[PROFILE] Received GetProfile request");
+                }
+                request if request == RequestCodes::GetPreferences as isize => {
+                    log::debug!("[PROFILE] Received GetPreferences request");
+                }
+                request if request == RequestCodes::GetRestrictions as isize => {
+                    log::debug!("[PROFILE] Received GetRestrictions request");
+                }
+                request if request == RequestCodes::GetTools as isize => {
+                    log::debug!("[PROFILE] Received GetTools request");
+                }
+                request if request == RequestCodes::GetIngredients as isize => {
+                    log::debug!("[PROFILE] Received GetIngredients request");
+                }
+                request if request == RequestCodes::GetLocation as isize => {
+                    log::debug!("[PROFILE] Received GetLocation request");
+                }
+                request if request == RequestCodes::EditProfile as isize => {
+                    log::debug!("[PROFILE] Received EditProfile request");
+                }
+                request => log::warn!("[PROFILE] Received unsupported request code: {request}"),
             }
         } else {
             // Match call from user/webclient
-            todo!()
+            log::debug!("[PROFILE] Received request from external address!");
         }
     }
+}
+
+struct Profile {
+    uid: u32,
+    preferences: Vec<String>,
+    restrictions: Vec<String>,
+    tools: Vec<String>,
+    ingredients: Vec<String>,
+    location: Vec<String>,
+}
+
+fn fetch_profile(_user_id: u32, _client: &SupabaseClient) -> Option<Profile> {
+    None
 }
